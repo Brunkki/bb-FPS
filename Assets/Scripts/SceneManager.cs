27246a254ac60eb54fa397ab85afc90b340a1759
Brunkki <zagorac.bruno@gmail.com>
2021-05-04 19:30:13 +0300

    Vähän kenttädesigniä

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
